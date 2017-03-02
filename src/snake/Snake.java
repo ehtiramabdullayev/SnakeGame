098ad72bd9a88cdcd54a03c99add1dcc1ae492eb
Master <Master@DESKTOP-GITDDKD>
2017-03-02 21:54:02 +0400

    --added some dot for snake

--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -1,16 +1,33 @@
 package snake;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Master
  */
-public class Snake {
+public class Snake extends  JFrame{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Rectangle  rectangle = new Rectangle(400, 600);
+        PointObj obj = new PointObj();
+       //// JPanel jPanel = new JPanel();
+       // jPanel.paint();
+        
+        
+        Snake snake= new Snake();
+        
+        snake.setBounds(rectangle);
+        snake.setVisible(true);
+        snake.add(obj);
     }
     
 }
